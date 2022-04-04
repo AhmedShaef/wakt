71b005a51d46f1a7ed94ebad3239c8e610ce0d31
Ahmed Shaef <shaef@outlook.com>
2022-04-04 19:53:10 +0300

    change: tags to tag

--- foundation/web/request.go
+++ foundation/web/request.go
@@ -16,7 +16,7 @@ func Param(r *http.Request, key string) string {
 // Decode reads the body of an HTTP request looking for a JSON document. The
 // body is decoded into the provided value.
 //
-// If the provided value is a struct then it is checked for validation tags.
+// If the provided value is a struct then it is checked for validation tag.
 func Decode(r *http.Request, val interface{}) error {
 	decoder := json.NewDecoder(r.Body)
 	decoder.DisallowUnknownFields()
