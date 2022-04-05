eacedcb5cbca53ec72de0d95f8b80bc8f8bb3484
Ahmed Shaef <shaef@outlook.com>
2022-04-05 23:02:49 +0300

    add: core client models package

---
+++ business/core/client/models.go
@@ -0,0 +1,49 @@
+package client
+
+import (
+	"github.com/AhmedShaef/wakt/business/core/client/db"
+	"time"
+	"unsafe"
+)
+
+// Client represents an individual client.
+type Client struct {
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Wid         string    `json:"wid"`
+	Notes       string    `json:"notes"`
+	DateUpdated time.Time `json:"date_updated"`
+}
+
+// NewClient contains information needed to create a new client.
+type NewClient struct {
+	Name  string `json:"name" validate:"required"`
+	Wid   string `json:"wid"`
+	Notes string `json:"notes"`
+}
+
+// UpdateClient defines what information may be provided to modify an existing
+// client. All fields are optional so clients can send just the fields they want
+// changed. It uses pointer fields ,so we can differentiate between a field that
+// was not provided and a field that was provided as explicitly blank. Normally
+// we do not want to use pointers to basic types ,but we make exceptions around
+// marshalling/unmarshalling.
+type UpdateClient struct {
+	Name  *string `json:"name"`
+	Notes *string `json:"notes"`
+}
+
+// =============================================================================
+
+func toClient(dbClient db.Client) Client {
+	pu := (*Client)(unsafe.Pointer(&dbClient))
+	return *pu
+}
+
+func toClientsSlice(dbClient []db.Client) []Client {
+	clients := make([]Client, len(dbClient))
+	for i, dbclint := range dbClient {
+		clients[i] = toClient(dbclint)
+	}
+	return clients
+}
