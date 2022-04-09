52fbfb47e8bc15533b7446484a7c2d03096276da
Ahmed Shaef <shaef@outlook.com>
2022-04-09 23:13:51 +0300

    add: business core tag models package

---
+++ business/core/tag/models.go
@@ -0,0 +1,44 @@
+package tag
+
+import (
+	"github.com/AhmedShaef/wakt/business/core/tag/db"
+	"unsafe"
+)
+
+// Tag represents an individual tag.
+type Tag struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Wid  string `json:"wid"`
+}
+
+// NewTag contains information needed to create a new tag.
+type NewTag struct {
+	Name string `json:"name" validate:"required"`
+	Wid  string `json:"wid" validate:"required"`
+}
+
+// UpdateTag defines what information may be provided to modify an existing
+// tag. All fields are optional so tag can send just the fields they want
+// changed. It uses pointer fields ,so we can differentiate between a field that
+// was not provided and a field that was provided as explicitly blank. Normally
+// we do not want to use pointers to basic types ,but we make exceptions around
+// marshalling/unmarshalling.
+type UpdateTag struct {
+	Name *string `json:"name"`
+}
+
+// =============================================================================
+
+func toTag(dbTag db.Tag) Tag {
+	pu := (*Tag)(unsafe.Pointer(&dbTag))
+	return *pu
+}
+
+func toTagsSlice(dbTag []db.Tag) []Tag {
+	tags := make([]Tag, len(dbTag))
+	for i, dbtag := range dbTag {
+		tags[i] = toTag(dbtag)
+	}
+	return tags
+}
