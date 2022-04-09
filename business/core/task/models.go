17d4c23206ad4c8c767ad3fd9b0a20bd440ab712
Ahmed Shaef <shaef@outlook.com>
2022-04-09 23:16:10 +0300

    add: business core task model package

---
+++ business/core/task/models.go
@@ -0,0 +1,59 @@
+package task
+
+import (
+	"github.com/AhmedShaef/wakt/business/core/task/db"
+	"time"
+	"unsafe"
+)
+
+// Task represents an individual task.
+type Task struct {
+	ID               string        `json:"id"`
+	Name             string        `json:"name"`
+	Pid              string        `json:"pid"`
+	Wid              string        `json:"wid"`
+	Uid              string        `json:"uid"`
+	EstimatedSeconds time.Duration `json:"estimated_seconds"`
+	Active           bool          `json:"active"`
+	DateUpdated      time.Time     `json:"date_updated"`
+	TrackedSeconds   time.Duration `json:"tracked_seconds"`
+}
+
+// NewTask contains information needed to create a new task.
+type NewTask struct {
+	Name             string        `json:"name" validate:"required"`
+	Pid              string        `json:"pid" validate:"required"`
+	Wid              string        `json:"wid"`
+	Uid              string        `json:"uid"`
+	EstimatedSeconds time.Duration `json:"estimated_seconds"`
+	Active           bool          `json:"active"`
+	TrackedSeconds   time.Duration `json:"tracked_seconds"`
+}
+
+// UpdateTask defines what information may be provided to modify an existing
+// task. All fields are optional so tasks can send just the fields they want
+// changed. It uses pointer fields ,so we can differentiate between a field that
+// was not provided and a field that was provided as explicitly blank. Normally
+// we do not want to use pointers to basic types ,but we make exceptions around
+// marshalling/unmarshalling.
+type UpdateTask struct {
+	Name             *string        `json:"name"`
+	EstimatedSeconds *time.Duration `json:"estimated_seconds"`
+	Active           *bool          `json:"active"`
+	TrackedSeconds   *time.Duration `json:"tracked_seconds"`
+}
+
+// =============================================================================
+
+func toTask(dbtask db.Task) Task {
+	pu := (*Task)(unsafe.Pointer(&dbtask))
+	return *pu
+}
+
+func toTasksSlice(dbtask []db.Task) []Task {
+	tasks := make([]Task, len(dbtask))
+	for i, dbtask := range dbtask {
+		tasks[i] = toTask(dbtask)
+	}
+	return tasks
+}
