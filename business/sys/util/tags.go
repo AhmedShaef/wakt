52548443904358f29b4b2e348ab0b6f348488c26
Ahmed Shaef <shaef@outlook.com>
2022-04-21 23:05:47 +0300

    update: letter case

--- business/sys/util/tags.go
+++ business/sys/util/tags.go
@@ -1,20 +1,21 @@
+// Package util provides some common functions used in the system.
 package util
 
-// Add bulk tags to a tagset.
-func Add(oldtags, tags []string) []string {
+// Add bulk tags to a tagSet.
+func Add(oldTag, tags []string) []string {
 	newTags := []string{}
 	for _, tag := range tags {
-		newTags = append(oldtags, tag)
+		newTags = append(oldTag, tag)
 	}
 	return newTags
 }
 
-// Remove bulk tags from a tagset.
-func Remove(oldtags, tags []string) []string {
+// Remove bulk tags from a tagSet.
+func Remove(oldTag, tags []string) []string {
 	newTags := []string{}
 	for _, tag := range tags {
-		for _, oldtag := range oldtags {
-			if oldtag != tag {
+		for _, oldTag := range oldTag {
+			if oldTag != tag {
 				newTags = append(newTags, tag)
 			}
 		}
