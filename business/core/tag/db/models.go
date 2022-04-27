5306a1f6680cf4155b5a9c414a1b8d4a506f2448
Ahmed Shaef <shaef@outlook.com>
2022-04-27 22:36:32 +0300

    add: date created and date update

--- business/core/tag/db/models.go
+++ business/core/tag/db/models.go
@@ -1,9 +1,13 @@
 package db
 
+import "time"
+
 // Tag represent the structure we need for moving data
 // between the app and the database.
 type Tag struct {
-	ID   string `db:"tag_id"`
-	Name string `db:"name"`
-	Wid  string `db:"wid"`
+	ID          string    `db:"tag_id"`
+	Name        string    `db:"name"`
+	Wid         string    `db:"wid"`
+	DateCreated time.Time `db:"date_created"`
+	DateUpdated time.Time `db:"date_updated"`
 }
