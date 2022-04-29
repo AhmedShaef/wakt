c4fc35d6c51f3815cf65ef33b62899474214eef5
Ahmed Shaef <shaef@outlook.com>
2022-04-30 00:41:01 +0300

    add: core project user db model

---
+++ business/core/project_user/db/models.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	"time"
+)
+
+// ProjectUser represent the structure we need for moving data
+// between the app and the database.
+type ProjectUser struct {
+	ID          string    `db:"project_user_id"`
+	Pid         string    `db:"pid"`
+	Uid         string    `db:"uid"`
+	Wid         string    `db:"wid"`
+	Rate        float64   `db:"rate"`
+	DateCreated time.Time `db:"date_created"`
+	DateUpdated time.Time `db:"date_updated"`
+}
