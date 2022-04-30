472f6b769e5ffec17937ef0deab735dd5b7c2a2f
Ahmed Shaef <shaef@outlook.com>
2022-05-01 00:33:21 +0300

    fix: unused vars

--- business/core/project/db/models.go
+++ business/core/project/db/models.go
@@ -11,13 +11,11 @@ type Project struct {
 	Cid            string        `db:"cid"`
 	Active         bool          `db:"active"`
 	IsPrivate      bool          `db:"is_private"`
-	Template       bool          `db:"template"`
-	TemplateID     string        `db:"template_id"`
 	Billable       bool          `db:"billable"`
 	AutoEstimates  bool          `db:"auto_estimates"`
 	EstimatedHours time.Duration `db:"estimated_hours"`
-	DateUpdated    time.Time     `db:"date_updated"`
-	Color          int           `db:"color"`
-	Rate           float32       `db:"rate"`
 	DateCreated    time.Time     `db:"date_created"`
+	DateUpdated    time.Time     `db:"date_updated"`
+	Rate           float32       `db:"rate"`
+	HexColor       string        `db:"hex_color"`
 }
