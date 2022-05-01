16dc00fb8ecf72f8472bfa30b48fc19e874b7364
Ahmed Shaef <shaef@outlook.com>
2022-05-02 00:02:47 +0300

    add: core time entry db models

--- business/core/time_entry/db/models.go
+++ business/core/time_entry/db/models.go
@@ -1,25 +1,26 @@
 package db
 
 import (
+	"github.com/lib/pq"
 	"time"
 )
 
-// TimeEntrie represent the structure we need for moving data
+// TimeEntry represent the structure we need for moving data
 // between the app and the database.
-type TimeEntrie struct {
-	ID          string        `db:"time_entrie_id"`
-	Description string        `db:"description"`
-	Uid         string        `db:"uid"`
-	Wid         string        `db:"wid"`
-	Pid         string        `db:"pid"`
-	Tid         string        `db:"tid"`
-	Billable    bool          `db:"billable"`
-	Start       time.Time     `db:"start"`
-	Stop        time.Time     `db:"stop"`
-	Duration    time.Duration `db:"duration"`
-	CreatedWith string        `db:"created_with"`
-	Tags        []string      `db:"tags"`
-	DurOnly     bool          `db:"dur_only"`
-	DateCreated time.Time     `db:"date_created"`
-	DateUpdated time.Time     `db:"date_updated"`
+type TimeEntry struct {
+	ID          string         `db:"time_entry_id"`
+	Description string         `db:"description"`
+	Uid         string         `db:"uid"`
+	Wid         string         `db:"wid"`
+	Pid         string         `db:"pid"`
+	Tid         string         `db:"tid"`
+	Billable    bool           `db:"billable"`
+	Start       time.Time      `db:"start"`
+	Stop        time.Time      `db:"stop"`
+	Duration    time.Duration  `db:"duration"`
+	CreatedWith string         `db:"created_with"`
+	Tags        pq.StringArray `db:"tags"`
+	DurOnly     bool           `db:"dur_only"`
+	DateCreated time.Time      `db:"date_created"`
+	DateUpdated time.Time      `db:"date_updated"`
 }
