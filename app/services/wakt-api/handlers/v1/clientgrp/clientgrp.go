44a8094258a3d4b629cb39848e219041d6bccad2
Ahmed Shaef <shaef@outlook.com>
2022-05-24 23:02:52 +0300

    update: authorization

---
+++ app/services/wakt-api/handlers/v1/clientgrp/clientgrp.go
@@ -0,0 +1,271 @@
+// Package clientgrp maintains the group of handlers for group access.
+package clientgrp
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/AhmedShaef/wakt/business/core/client"
+	"github.com/AhmedShaef/wakt/business/core/project"
+	"github.com/AhmedShaef/wakt/business/core/workspace"
+	"github.com/AhmedShaef/wakt/business/sys/auth"
+	v1Web "github.com/AhmedShaef/wakt/business/web/v1"
+	"github.com/AhmedShaef/wakt/foundation/web"
+	"net/http"
+	"strconv"
+)
+
+// Handlers manages the set of client endpoints.
+type Handlers struct {
+	Client    client.Core
+	Workspace workspace.Core
+	Project   project.Core
+}
+
+// Create adds a new client to the system.
+func (h Handlers) Create(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	v, err := web.GetValues(ctx)
+	if err != nil {
+		return web.NewShutdownError("web value missing from context")
+	}
+
+	claims, err := auth.GetClaims(ctx)
+	if err != nil {
+		return v1Web.NewRequestError(auth.ErrForbidden, http.StatusForbidden)
+	}
+
+	var nu client.NewClient
+	if err := web.Decode(r, &nu); err != nil {
+		return fmt.Errorf("unable to decode payload: %w", err)
+	}
+
+	workspaces, err := h.Workspace.QueryByID(ctx, nu.Wid)
+	if err != nil {
+		switch {
+		case errors.Is(err, workspace.ErrInvalidID):
+			return v1Web.NewRequestError(err, http.StatusBadRequest)
+		case errors.Is(err, workspace.ErrNotFound):
+			return v1Web.NewRequestError(err, http.StatusNotFound)
+		default:
+			return fmt.Errorf("querying workspace[%s]: %w", nu.Wid, err)
+		}
+	}
+
+	// If you are not an admin and looking to update a client you don't own.
+	if workspaces.Uid != claims.Subject {
+		return v1Web.NewRequestError(auth.ErrForbidden, http.StatusForbidden)
+	}
+
+	clint, err := h.Client.Create(ctx, nu, workspaces.Uid, v.Now)
+	if err != nil {
+		switch {
+		case errors.Is(err, client.ErrInvalidID):
+			return v1Web.NewRequestError(err, http.StatusBadRequest)
+		case errors.Is(err, client.ErrNotFound):
+			return v1Web.NewRequestError(err, http.StatusNotFound)
+		default:
+			return fmt.Errorf("client[%+v]: %w", &clint, err)
+		}
+	}
+
+	return web.Respond(ctx, w, clint, http.StatusCreated)
+}
+
+// Update updates a client in the system.
+func (h Handlers) Update(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	v, err := web.GetValues(ctx)
+	if err != nil {
+		return web.NewShutdownError("web value missing from context")
+	}
+
+	claims, err := auth.GetClaims(ctx)
+	if err != nil {
+		return v1Web.NewRequestError(auth.ErrForbidden, http.StatusForbidden)
+	}
+
+	var upd client.UpdateClient
+	if err := web.Decode(r, &upd); err != nil {
+		return fmt.Errorf("unable to decode payload: %w", err)
+	}
+
+	clientID := web.Param(r, "id")
+
+	clients, err := h.Client.QueryByID(ctx, clientID)
+	if err != nil {
+		switch {
+		case errors.Is(err, client.ErrInvalidID):
+			return v1Web.NewRequestError(err, http.StatusBadRequest)
+		case errors.Is(err, client.ErrNotFound):
+			return v1Web.NewRequestError(err, http.StatusNotFound)
+		default:
+			return fmt.Errorf("querying workspace[%s]: %w", clientID, err)
+		}
+	}
+
+	// If you are not an admin and looking to retrieve someone other than yourself.
+	if claims.Subject != clients.Uid {
+		return v1Web.NewRequestError(auth.ErrForbidden, http.StatusForbidden)
+	}
+
+	if err := h.Client.Update(ctx, clientID, upd, v.Now); err != nil {
+		switch {
+		case errors.Is(err, client.ErrInvalidID):
+			return v1Web.NewRequestError(err, http.StatusBadRequest)
+		case errors.Is(err, client.ErrNotFound):
+			return v1Web.NewRequestError(err, http.StatusNotFound)
+		default:
+			return fmt.Errorf("ID[%s] Client[%+v]: %w", clientID, &upd, err)
+		}
+	}
+
+	return web.Respond(ctx, w, nil, http.StatusNoContent)
+}
+
+// Delete removes a client from the system.
+func (h Handlers) Delete(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	claims, err := auth.GetClaims(ctx)
+	if err != nil {
+		return v1Web.NewRequestError(auth.ErrForbidden, http.StatusForbidden)
+	}
+
+	clientID := web.Param(r, "id")
+
+	clients, err := h.Client.QueryByID(ctx, clientID)
+	if err != nil {
+		switch {
+		case errors.Is(err, client.ErrInvalidID):
+			return v1Web.NewRequestError(err, http.StatusBadRequest)
+		case errors.Is(err, client.ErrNotFound):
+			return v1Web.NewRequestError(err, http.StatusNotFound)
+		default:
+			return fmt.Errorf("querying workspace[%s]: %w", clientID, err)
+		}
+	}
+
+	// If you are not an admin and looking to retrieve someone other than yourself.
+	if claims.Subject != clients.Uid {
+		return v1Web.NewRequestError(auth.ErrForbidden, http.StatusForbidden)
+	}
+
+	if err := h.Client.Delete(ctx, clientID); err != nil {
+		switch {
+		case errors.Is(err, client.ErrInvalidID):
+			return v1Web.NewRequestError(err, http.StatusBadRequest)
+		default:
+			return fmt.Errorf("ID[%s]: %w", clientID, err)
+		}
+	}
+
+	return web.Respond(ctx, w, nil, http.StatusNoContent)
+}
+
+// Query returns a list of clients with paging.
+func (h Handlers) Query(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	claims, err := auth.GetClaims(ctx)
+	if err != nil {
+		return v1Web.NewRequestError(auth.ErrForbidden, http.StatusForbidden)
+	}
+
+	page := web.Param(r, "page")
+	pageNumber, err := strconv.Atoi(page)
+	if err != nil {
+		return v1Web.NewRequestError(fmt.Errorf("invalid page format, page[%s]", page), http.StatusBadRequest)
+	}
+	rows := web.Param(r, "rows")
+	rowsPerPage, err := strconv.Atoi(rows)
+	if err != nil {
+		return v1Web.NewRequestError(fmt.Errorf("invalid rows format, rows[%s]", rows), http.StatusBadRequest)
+	}
+
+	clint, err := h.Client.Query(ctx, claims.Subject, pageNumber, rowsPerPage)
+	if err != nil {
+		return fmt.Errorf("unable to query for clients: %w", err)
+	}
+
+	return web.Respond(ctx, w, clint, http.StatusOK)
+}
+
+// QueryByID returns a client by its ID.
+func (h Handlers) QueryByID(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	claims, err := auth.GetClaims(ctx)
+	if err != nil {
+		return v1Web.NewRequestError(auth.ErrForbidden, http.StatusForbidden)
+	}
+
+	clientID := web.Param(r, "id")
+
+	clients, err := h.Client.QueryByID(ctx, clientID)
+	if err != nil {
+		switch {
+		case errors.Is(err, client.ErrInvalidID):
+			return v1Web.NewRequestError(err, http.StatusBadRequest)
+		case errors.Is(err, client.ErrNotFound):
+			return v1Web.NewRequestError(err, http.StatusNotFound)
+		default:
+			return fmt.Errorf("querying workspace[%s]: %w", clientID, err)
+		}
+	}
+
+	// If you are not an admin and looking to retrieve someone other than yourself.
+	if claims.Subject != clients.Uid {
+		return v1Web.NewRequestError(auth.ErrForbidden, http.StatusForbidden)
+	}
+
+	clint, err := h.Client.QueryByID(ctx, clientID)
+	if err != nil {
+		switch {
+		case errors.Is(err, client.ErrInvalidID):
+			return v1Web.NewRequestError(err, http.StatusBadRequest)
+		case errors.Is(err, client.ErrNotFound):
+			return v1Web.NewRequestError(err, http.StatusNotFound)
+		default:
+			return fmt.Errorf("ID[%s]: %w", clientID, err)
+		}
+	}
+
+	return web.Respond(ctx, w, clint, http.StatusOK)
+}
+
+// QueryClientProjects returns a list of workspaces with paging.
+func (h Handlers) QueryClientProjects(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	claims, err := auth.GetClaims(ctx)
+	if err != nil {
+		return v1Web.NewRequestError(auth.ErrForbidden, http.StatusForbidden)
+	}
+
+	clientID := web.Param(r, "id")
+	page := web.Param(r, "page")
+	pageNumber, err := strconv.Atoi(page)
+	if err != nil {
+		return v1Web.NewRequestError(fmt.Errorf("invalid page format, page[%s]", page), http.StatusBadRequest)
+	}
+	rows := web.Param(r, "rows")
+	rowsPerPage, err := strconv.Atoi(rows)
+	if err != nil {
+		return v1Web.NewRequestError(fmt.Errorf("invalid rows format, rows[%s]", rows), http.StatusBadRequest)
+	}
+
+	clients, err := h.Workspace.QueryByID(ctx, clientID)
+	if err != nil {
+		switch {
+		case errors.Is(err, client.ErrInvalidID):
+			return v1Web.NewRequestError(err, http.StatusBadRequest)
+		case errors.Is(err, client.ErrNotFound):
+			return v1Web.NewRequestError(err, http.StatusNotFound)
+		default:
+			return fmt.Errorf("querying client[%s]: %w", clientID, err)
+		}
+	}
+
+	// If you are not an admin and looking to retrieve someone other than yourself.
+	if claims.Subject != clients.Uid {
+		return v1Web.NewRequestError(auth.ErrForbidden, http.StatusForbidden)
+	}
+
+	work, err := h.Project.QueryClientProjects(ctx, clientID, pageNumber, rowsPerPage)
+	if err != nil {
+		return fmt.Errorf("unable to query for clients: %w", err)
+	}
+
+	return web.Respond(ctx, w, work, http.StatusOK)
+}
