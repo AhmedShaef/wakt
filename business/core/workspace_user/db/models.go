146586dbaf3a099484316a36332d51de638a1dcf
Ahmed Shaef <shaef@outlook.com>
2022-05-27 22:55:19 +0300

    add: create and updated date

--- business/core/workspace_user/db/models.go
+++ business/core/workspace_user/db/models.go
@@ -1,11 +1,16 @@
 package db
 
+import "time"
+
 // WorkspaceUser represent the structure we need for moving data
 // between the app and the database.
 type WorkspaceUser struct {
-	ID        string `db:"workspace_user_id"`
-	Uid       string `db:"uid"`
-	Wid       string `db:"wid"`
-	Active    bool   `db:"active"`
-	InviteKey string `db:"invite_key"`
+	ID          string    `db:"workspace_user_id"`
+	Uid         string    `db:"uid"`
+	Wid         string    `db:"wid"`
+	Admin       bool      `db:"admin"`
+	Active      bool      `db:"active"`
+	InviteKey   string    `db:"invite_key"`
+	DateCreated time.Time `db:"date_created"`
+	DateUpdated time.Time `db:"date_updated"`
 }
