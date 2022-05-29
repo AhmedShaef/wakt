50bb9ecce6f5d9e6a38aec94ca7191caf0c79387
Ahmed Shaef <shaef@outlook.com>
2022-05-29 08:49:26 +0300

    add: default functionality to db struct.

--- business/core/project/db/models.go
+++ business/core/project/db/models.go
@@ -8,14 +8,14 @@ type Project struct {
 	ID             string        `db:"project_id"`
 	Name           string        `db:"name"`
 	Wid            string        `db:"wid"`
-	Cid            string        `db:"cid"`
-	Active         bool          `db:"active"`
-	IsPrivate      bool          `db:"is_private"`
-	Billable       bool          `db:"billable"`
-	AutoEstimates  bool          `db:"auto_estimates"`
-	EstimatedHours time.Duration `db:"estimated_hours"`
+	Cid            string        `db:"cid" default:""`
+	Active         bool          `db:"active" default:"true"`
+	IsPrivate      bool          `db:"is_private" default:"true"`
+	Billable       bool          `db:"billable" default:"true"`
+	AutoEstimates  bool          `db:"auto_estimates" default:"false"`
+	EstimatedHours time.Duration `db:"estimated_hours" default:""`
 	DateCreated    time.Time     `db:"date_created"`
 	DateUpdated    time.Time     `db:"date_updated"`
-	Rate           float32       `db:"rate"`
-	HexColor       string        `db:"hex_color"`
+	Rate           float32       `db:"rate" default:""`
+	HexColor       string        `db:"hex_color" default:""`
 }
