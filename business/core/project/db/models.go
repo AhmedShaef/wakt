9c642c3ced4b164b4898100b314fb50cc32cbb45
Ahmed Shaef <shaef@outlook.com>
2022-06-03 22:37:54 +0300

    add: User id var to stract

--- business/core/project/db/models.go
+++ business/core/project/db/models.go
@@ -8,14 +8,15 @@ type Project struct {
 	ID             string        `db:"project_id"`
 	Name           string        `db:"name"`
 	Wid            string        `db:"wid"`
-	Cid            string        `db:"cid" default:""`
-	Active         bool          `db:"active" default:"true"`
-	IsPrivate      bool          `db:"is_private" default:"true"`
-	Billable       bool          `db:"billable" default:"true"`
-	AutoEstimates  bool          `db:"auto_estimates" default:"false"`
-	EstimatedHours time.Duration `db:"estimated_hours" default:""`
+	Cid            string        `db:"cid"`
+	Uid            string        `db:"uid"`
+	Active         bool          `db:"active"`
+	IsPrivate      bool          `db:"is_private"`
+	Billable       bool          `db:"billable"`
+	AutoEstimates  bool          `db:"auto_estimates"`
+	EstimatedHours time.Duration `db:"estimated_hours"`
 	DateCreated    time.Time     `db:"date_created"`
 	DateUpdated    time.Time     `db:"date_updated"`
-	Rate           float32       `db:"rate" default:""`
-	HexColor       string        `db:"hex_color" default:""`
+	Rate           float32       `db:"rate"`
+	HexColor       string        `db:"hex_color"`
 }
