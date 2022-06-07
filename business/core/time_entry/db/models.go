ff21de10dbb86a827dc3a62ecb37a5a8124bdbc1
Ahmed Shaef <shaef@outlook.com>
2022-06-07 23:09:34 +0300

    delete: default

--- business/core/time_entry/db/models.go
+++ business/core/time_entry/db/models.go
@@ -9,18 +9,18 @@ import (
 // between the app and the database.
 type TimeEntry struct {
 	ID          string         `db:"time_entry_id"`
-	Description string         `db:"description"`
+	Description string         `db:"description" default:"no description"`
 	Uid         string         `db:"uid"`
 	Wid         string         `db:"wid"`
-	Pid         string         `db:"pid"`
-	Tid         string         `db:"tid"`
-	Billable    bool           `db:"billable"`
+	Pid         string         `db:"pid" default:""`
+	Tid         string         `db:"tid" default:""`
+	Billable    bool           `db:"billable" default:"false"`
 	Start       time.Time      `db:"start"`
-	Stop        time.Time      `db:"stop"`
-	Duration    time.Duration  `db:"duration"`
+	Stop        time.Time      `db:"stop" default:""`
+	Duration    time.Duration  `db:"duration" default:"-1"`
 	CreatedWith string         `db:"created_with"`
-	Tags        pq.StringArray `db:"tags"`
-	DurOnly     bool           `db:"dur_only"`
+	Tags        pq.StringArray `db:"tags" default:"[]"`
+	DurOnly     bool           `db:"dur_only" default:"false"`
 	DateCreated time.Time      `db:"date_created"`
 	DateUpdated time.Time      `db:"date_updated"`
 }
