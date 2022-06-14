b82121acd9b242b18caa859d709cf247496c546e
Ahmed Shaef <shaef@outlook.com>
2022-06-14 23:07:41 +0300

    add: client integration test

--- app/services/wakt-api/tests/v1/client_test.go
+++ app/services/wakt-api/tests/v1/client_test.go
@@ -1,1 +1,490 @@
 package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/AhmedShaef/wakt/business/core/project"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/AhmedShaef/wakt/app/services/wakt-api/handlers"
+	"github.com/AhmedShaef/wakt/business/core/client"
+	"github.com/AhmedShaef/wakt/business/data/dbtest"
+	"github.com/AhmedShaef/wakt/business/sys/validate"
+	v1Web "github.com/AhmedShaef/wakt/business/web/v1"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+)
+
+// ClientTests holds methods for each client subtest. This type allows
+// passing dependencies for tests while still providing a convenient syntax
+// when subtests are registered.
+type ClientTests struct {
+	app       http.Handler
+	userToken string
+}
+
+// TestClients runs a series of tests to exercise Client behavior from the
+// API level. The subtests all share the same database and application for
+// speed and convenience. The downside is the order the tests are ran matters
+// and one test may break if other tests are not ran before it. If a particular
+// subtest needs a fresh instance of the application it can make it or it
+// should be its own Test* function.
+func TestClients(t *testing.T) {
+	t.Parallel()
+
+	test := dbtest.NewIntegration(t, c, "inttestclient")
+	t.Cleanup(test.Teardown)
+
+	shutdown := make(chan os.Signal, 1)
+	tests := ClientTests{
+		app: handlers.APIMux(handlers.APIMuxConfig{
+			Shutdown: shutdown,
+			Log:      test.Log,
+			Auth:     test.Auth,
+			DB:       test.DB,
+		}),
+		userToken: test.Token("admin@example.com", "gophers"),
+	}
+
+	t.Run("postClient400", tests.postClient400)
+	t.Run("postClient401", tests.postClient401)
+	t.Run("getClient404", tests.getClient404)
+	t.Run("getClient400", tests.getClient400)
+	t.Run("deleteClientNotFound", tests.deleteClientNotFound)
+	t.Run("putClient404", tests.putClient404)
+	t.Run("crudClients", tests.crudClient)
+}
+
+// postClient400 validates a client can't be created with the endpoint
+// unless a valid client document is submitted.
+func (pt *ClientTests) postClient400(t *testing.T) {
+	r := httptest.NewRequest(http.MethodPost, "/v1/client", strings.NewReader(`{}`))
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate a new client can't be created with an invalid document.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using an incomplete client value.", testID)
+		{
+			if w.Code != http.StatusBadRequest {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 400 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 400 for the response.", dbtest.Success, testID)
+
+			// Inspect the response.
+			var got v1Web.ErrorResponse
+			if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to unmarshal the response to an error type : %v", dbtest.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to unmarshal the response to an error type.", dbtest.Success, testID)
+
+			fields := validate.FieldErrors{
+				{Field: "name", Error: "name is a required field"},
+			}
+			exp := v1Web.ErrorResponse{
+				Error:  "data validation error",
+				Fields: fields.Fields(),
+			}
+
+			// We can't rely on the order of the field errors so they have to be
+			// sorted. Tell the cmp package how to sort them.
+			sorter := cmpopts.SortSlices(func(a, b validate.FieldError) bool {
+				return a.Field < b.Field
+			})
+
+			if diff := cmp.Diff(got, exp, sorter); diff != "" {
+				t.Fatalf("\t%s\tTest %d:\tShould get the expected result. Diff:\n%s", dbtest.Failed, testID, diff)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Success, testID)
+		}
+	}
+}
+
+// postClient401 validates a client can't be created with the endpoint
+// unless the user is authenticated
+func (pt *ClientTests) postClient401(t *testing.T) {
+	np := client.NewClient{
+		Name: "Comic Books",
+	}
+
+	body, err := json.Marshal(&np)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r := httptest.NewRequest(http.MethodPost, "/v1/client", bytes.NewBuffer(body))
+	w := httptest.NewRecorder()
+
+	// Not setting an authorization header.
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate a new client can't be created with an invalid document.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using an incomplete client value.", testID)
+		{
+			if w.Code != http.StatusUnauthorized {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 401 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 401 for the response.", dbtest.Success, testID)
+		}
+	}
+}
+
+// getClient400 validates a client request for a malformed id.
+func (pt *ClientTests) getClient400(t *testing.T) {
+	id := "12345"
+
+	r := httptest.NewRequest(http.MethodGet, "/v1/client/"+id, nil)
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate getting a client with a malformed id.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the new client %s.", testID, id)
+		{
+			if w.Code != http.StatusBadRequest {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 400 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 400 for the response.", dbtest.Success, testID)
+
+			got := w.Body.String()
+			exp := `{"error":"ID is not in its proper form"}`
+			if got != exp {
+				t.Logf("\t\tTest %d:\tGot : %v", testID, got)
+				t.Logf("\t\tTest %d:\tExp: %v", testID, exp)
+				t.Fatalf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Failed, testID)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Success, testID)
+		}
+	}
+}
+
+// getClient404 validates a client request for a client that does not exist with the endpoint.
+func (pt *ClientTests) getClient404(t *testing.T) {
+	id := "45cf87a3-5915-4079-a9af-6c559239ddbf"
+
+	r := httptest.NewRequest(http.MethodGet, "/v1/client/"+id, nil)
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate getting a client with an unknown id.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the new client %s.", testID, id)
+		{
+			if w.Code != http.StatusNotFound {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 404 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 404 for the response.", dbtest.Success, testID)
+
+			got := w.Body.String()
+			exp := "not found"
+			if !strings.Contains(got, exp) {
+				t.Logf("\t\tTest %d:\tGot : %v", testID, got)
+				t.Logf("\t\tTest %d:\tExp: %v", testID, exp)
+				t.Fatalf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Failed, testID)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Success, testID)
+		}
+	}
+}
+
+// deleteClientNotFound validates deleting a client that does not exist is not a failure.
+func (pt *ClientTests) deleteClientNotFound(t *testing.T) {
+	id := "112262f1-1a77-4374-9f22-39e575aa6348"
+
+	r := httptest.NewRequest(http.MethodDelete, "/v1/client/"+id, nil)
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate deleting a client that does not exist.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the new client %s.", testID, id)
+		{
+			if w.Code != http.StatusNotFound {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 404 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 204 for the response.", dbtest.Success, testID)
+		}
+	}
+}
+
+// putClient404 validates updating a client that does not exist.
+func (pt *ClientTests) putClient404(t *testing.T) {
+	id := "9b468f90-1cf1-4377-b3fa-68b450d632a0"
+
+	up := client.UpdateClient{
+		Name: dbtest.StringPointer("Nonexistent"),
+	}
+	body, err := json.Marshal(&up)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r := httptest.NewRequest(http.MethodPut, "/v1/client/"+id, bytes.NewBuffer(body))
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate updating a client that does not exist.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the new client %s.", testID, id)
+		{
+			if w.Code != http.StatusNotFound {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 404 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 404 for the response.", dbtest.Success, testID)
+
+			got := w.Body.String()
+			exp := "not found"
+			if !strings.Contains(got, exp) {
+				t.Logf("\t\tTest %d:\tGot : %v", testID, got)
+				t.Logf("\t\tTest %d:\tExp: %v", testID, exp)
+				t.Fatalf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Failed, testID)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Success, testID)
+		}
+	}
+}
+
+// crudClient performs a complete test of CRUD against the api.
+func (pt *ClientTests) crudClient(t *testing.T) {
+	p := pt.postClient201(t)
+	defer pt.deleteClient204(t, p.ID)
+
+	pt.getClient200(t, p.ID)
+	pt.getClients200(t)
+	pt.getClientProject200(t, p.ID)
+	pt.putClient204(t, p.ID)
+}
+
+// postClient201 validates a client can be created with the endpoint.
+func (pt *ClientTests) postClient201(t *testing.T) client.Client {
+	np := client.NewClient{
+		Name: "Comic Books",
+	}
+
+	body, err := json.Marshal(&np)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r := httptest.NewRequest(http.MethodPost, "/v1/client", bytes.NewBuffer(body))
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	// This needs to be returned for other dbtest.
+	var got client.Client
+
+	t.Log("Given the need to create a new client with the clients endpoint.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the declared client value.", testID)
+		{
+			if w.Code != http.StatusCreated {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 201 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 201 for the response.", dbtest.Success, testID)
+
+			if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to unmarshal the response : %v", dbtest.Failed, testID, err)
+			}
+
+			// Define what we wanted to receive. We will just trust the generated
+			// fields like ID and Dates so we copy p.
+			exp := got
+			exp.Name = "Comic Books"
+
+			if diff := cmp.Diff(got, exp); diff != "" {
+				t.Fatalf("\t%s\tTest %d:\tShould get the expected result. Diff:\n%s", dbtest.Failed, testID, diff)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Success, testID)
+		}
+	}
+
+	return got
+}
+
+// deleteClient200 validates deleting a client that does exist.
+func (pt *ClientTests) deleteClient204(t *testing.T, id string) {
+	r := httptest.NewRequest(http.MethodDelete, "/v1/client/"+id, nil)
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate deleting a client that does exist.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the new client %s.", testID, id)
+		{
+			if w.Code != http.StatusNoContent {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 204 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 204 for the response.", dbtest.Success, testID)
+		}
+	}
+}
+
+// getClient200 validates a client request for an existing id.
+func (pt *ClientTests) getClient200(t *testing.T, id string) {
+	r := httptest.NewRequest(http.MethodGet, "/v1/client/"+id, nil)
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate getting a client that exists.")
+	{
+		testID := 0
+		t.Logf("\tTest : %d\tWhen using the new client %s.", testID, id)
+		{
+			if w.Code != http.StatusOK {
+				t.Fatalf("\t%s\tTest : %d\tShould receive a status code of 200 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest : %d\tShould receive a status code of 200 for the response.", dbtest.Success, testID)
+
+			var got client.Client
+			if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+				t.Fatalf("\t%s\tTest : %d\tShould be able to unmarshal the response : %v", dbtest.Failed, testID, err)
+			}
+
+			// Define what we wanted to receive. We will just trust the generated
+			// fields like Dates so we copy p.
+			exp := got
+			exp.ID = id
+			exp.Name = "Comic Books"
+
+			if diff := cmp.Diff(got, exp); diff != "" {
+				t.Fatalf("\t%s\tTest : %d\tShould get the expected result. Diff:\n%s", dbtest.Failed, testID, diff)
+			}
+			t.Logf("\t%s\tTest : %d\tShould get the expected result.", dbtest.Success, testID)
+		}
+	}
+}
+
+// getClients200 validates a clients request for an existing id.
+func (pt *ClientTests) getClients200(t *testing.T) {
+	r := httptest.NewRequest(http.MethodGet, "/v1/client/1/20", nil)
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate getting a clients that exists.")
+	{
+		testID := 0
+		t.Logf("\tTest : %d\tWhen using the new clients.", testID)
+		{
+			if w.Code != http.StatusOK {
+				t.Fatalf("\t%s\tTest : %d\tShould receive a status code of 200 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest : %d\tShould receive a status code of 200 for the response.", dbtest.Success, testID)
+
+			var got []client.Client
+			if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+				t.Fatalf("\t%s\tTest : %d\tShould be able to unmarshal the response : %v", dbtest.Failed, testID, err)
+			}
+
+			// Define what we wanted to receive. We will just trust the generated
+			// fields like Dates so we copy p.
+			exp := got
+			exp[0].Name = "Comic Books"
+
+			if diff := cmp.Diff(got, exp); diff != "" {
+				t.Fatalf("\t%s\tTest : %d\tShould get the expected result. Diff:\n%s", dbtest.Failed, testID, diff)
+			}
+			t.Logf("\t%s\tTest : %d\tShould get the expected result.", dbtest.Success, testID)
+		}
+	}
+}
+
+// getClientProject200 validates a client request for an existing id.
+func (pt *ClientTests) getClientProject200(t *testing.T, id string) {
+	r := httptest.NewRequest(http.MethodGet, "/v1/client/"+id+"/project/1/20", nil)
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate getting a client project that exists.")
+	{
+		testID := 0
+		t.Logf("\tTest : %d\tWhen using the new client %s.", testID, id)
+		{
+			if w.Code != http.StatusOK {
+				t.Fatalf("\t%s\tTest : %d\tShould receive a status code of 200 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest : %d\tShould receive a status code of 200 for the response.", dbtest.Success, testID)
+
+			var got []project.Project
+			if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+				t.Fatalf("\t%s\tTest : %d\tShould be able to unmarshal the response : %v", dbtest.Failed, testID, err)
+			}
+
+		}
+	}
+}
+
+// putClient204 validates updating a client that does exist.
+func (pt *ClientTests) putClient204(t *testing.T, id string) {
+	body := `{"name": "Graphic Novels"}`
+	r := httptest.NewRequest(http.MethodPut, "/v1/client/"+id, strings.NewReader(body))
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to update a client with the clients endpoint.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the modified client value.", testID)
+		{
+			if w.Code != http.StatusNoContent {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 204 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 204 for the response.", dbtest.Success, testID)
+
+			r = httptest.NewRequest(http.MethodGet, "/v1/client/"+id, nil)
+			w = httptest.NewRecorder()
+
+			r.Header.Set("Authorization", "Bearer "+pt.userToken)
+			pt.app.ServeHTTP(w, r)
+
+			if w.Code != http.StatusOK {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 200 for the retrieve : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 200 for the retrieve.", dbtest.Success, testID)
+
+			var ru client.Client
+			if err := json.NewDecoder(w.Body).Decode(&ru); err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to unmarshal the response : %v", dbtest.Failed, testID, err)
+			}
+
+			if ru.Name != "Graphic Novels" {
+				t.Fatalf("\t%s\tTest %d:\tShould see an updated Name : got %q want %q", dbtest.Failed, testID, ru.Name, "Graphic Novels")
+			}
+			t.Logf("\t%s\tTest %d:\tShould see an updated Name.", dbtest.Success, testID)
+		}
+	}
+}
