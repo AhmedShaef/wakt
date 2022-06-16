56f4113286f84fcede7386f5a1b13ad114578054
Ahmed Shaef <shaef@outlook.com>
2022-06-16 10:02:04 +0300

    add: task handlers package test

--- app/services/wakt-api/tests/v1/task_test.go
+++ app/services/wakt-api/tests/v1/task_test.go
@@ -1,1 +1,430 @@
 package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/AhmedShaef/wakt/app/services/wakt-api/handlers"
+	"github.com/AhmedShaef/wakt/business/core/task"
+	"github.com/AhmedShaef/wakt/business/data/dbtest"
+	"github.com/AhmedShaef/wakt/business/sys/validate"
+	v1Web "github.com/AhmedShaef/wakt/business/web/v1"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+)
+
+// TaskTests holds methods for each task subtest. This type allows
+// passing dependencies for tests while still providing a convenient syntax
+// when subtests are registered.
+type TaskTests struct {
+	app       http.Handler
+	userToken string
+}
+
+// TestTasks runs a series of tests to exercise Task behavior from the
+// API level. The subtests all share the same database and application for
+// speed and convenience. The downside is the order the tests are ran matters
+// and one test may break if other tests are not ran before it. If a particular
+// subtest needs a fresh instance of the application it can make it or it
+// should be its own Test* function.
+func TestTasks(t *testing.T) {
+	t.Parallel()
+
+	test := dbtest.NewIntegration(t, c, "inttesttask")
+	t.Cleanup(test.Teardown)
+
+	shutdown := make(chan os.Signal, 1)
+	tests := TaskTests{
+		app: handlers.APIMux(handlers.APIMuxConfig{
+			Shutdown: shutdown,
+			Log:      test.Log,
+			Auth:     test.Auth,
+			DB:       test.DB,
+		}),
+		userToken: test.Token("admin@example.com", "gophers"),
+	}
+
+	t.Run("postTask400", tests.postTask400)
+	t.Run("postTask401", tests.postTask401)
+	t.Run("getTask404", tests.getTask404)
+	t.Run("getTask400", tests.getTask400)
+	t.Run("deleteTaskNotFound", tests.deleteTaskNotFound)
+	t.Run("putTask404", tests.putTask404)
+	t.Run("crudTasks", tests.crudTask)
+}
+
+// postTask400 validates a task can't be created with the endpoint
+// unless a valid task document is submitted.
+func (pt *TaskTests) postTask400(t *testing.T) {
+	r := httptest.NewRequest(http.MethodPost, "/v1/task", strings.NewReader(`{}`))
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate a new task can't be created with an invalid document.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using an incomplete task value.", testID)
+		{
+			if w.Code != http.StatusBadRequest {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 400 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 400 for the response.", dbtest.Success, testID)
+
+			// Inspect the response.
+			var got v1Web.ErrorResponse
+			if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to unmarshal the response to an error type : %v", dbtest.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to unmarshal the response to an error type.", dbtest.Success, testID)
+
+			fields := validate.FieldErrors{
+				{Field: "name", Error: "name is a required field"},
+				{Field: "pid", Error: "pid is a required field"},
+			}
+			exp := v1Web.ErrorResponse{
+				Error:  "data validation error",
+				Fields: fields.Fields(),
+			}
+
+			// We can't rely on the order of the field errors so they have to be
+			// sorted. Tell the cmp package how to sort them.
+			sorter := cmpopts.SortSlices(func(a, b validate.FieldError) bool {
+				return a.Field < b.Field
+			})
+
+			if diff := cmp.Diff(got, exp, sorter); diff != "" {
+				t.Fatalf("\t%s\tTest %d:\tShould get the expected result. Diff:\n%s", dbtest.Failed, testID, diff)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Success, testID)
+		}
+	}
+}
+
+// postTask401 validates a task can't be created with the endpoint
+// unless the user is authenticated
+func (pt *TaskTests) postTask401(t *testing.T) {
+	np := task.NewTask{
+		Name: "Comic Books",
+		Pid:  "45cf87a3-5915-4079-a9af-6c559239ddbf",
+	}
+
+	body, err := json.Marshal(&np)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r := httptest.NewRequest(http.MethodPost, "/v1/task", bytes.NewBuffer(body))
+	w := httptest.NewRecorder()
+
+	// Not setting an authorization header.
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate a new task can't be created with an invalid document.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using an incomplete task value.", testID)
+		{
+			if w.Code != http.StatusUnauthorized {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 401 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 401 for the response.", dbtest.Success, testID)
+		}
+	}
+}
+
+// getTask400 validates a task request for a malformed id.
+func (pt *TaskTests) getTask400(t *testing.T) {
+	id := "12345"
+
+	r := httptest.NewRequest(http.MethodGet, "/v1/task/"+id, nil)
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate getting a task with a malformed id.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the new task %s.", testID, id)
+		{
+			if w.Code != http.StatusBadRequest {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 400 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 400 for the response.", dbtest.Success, testID)
+
+			got := w.Body.String()
+			exp := `{"error":"ID is not in its proper form"}`
+			if got != exp {
+				t.Logf("\t\tTest %d:\tGot : %v", testID, got)
+				t.Logf("\t\tTest %d:\tExp: %v", testID, exp)
+				t.Fatalf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Failed, testID)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Success, testID)
+		}
+	}
+}
+
+// getTask404 validates a task request for a task that does not exist with the endpoint.
+func (pt *TaskTests) getTask404(t *testing.T) {
+	id := "45cf87a3-5915-4079-a9af-6c559239ddbf"
+
+	r := httptest.NewRequest(http.MethodGet, "/v1/task/"+id, nil)
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate getting a task with an unknown id.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the new task %s.", testID, id)
+		{
+			if w.Code != http.StatusNotFound {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 404 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 404 for the response.", dbtest.Success, testID)
+
+			got := w.Body.String()
+			exp := "not found"
+			if !strings.Contains(got, exp) {
+				t.Logf("\t\tTest %d:\tGot : %v", testID, got)
+				t.Logf("\t\tTest %d:\tExp: %v", testID, exp)
+				t.Fatalf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Failed, testID)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Success, testID)
+		}
+	}
+}
+
+// deleteTaskNotFound validates deleting a task that does not exist is not a failure.
+func (pt *TaskTests) deleteTaskNotFound(t *testing.T) {
+	id := "112262f1-1a77-4374-9f22-39e575aa6348"
+
+	r := httptest.NewRequest(http.MethodDelete, "/v1/task/"+id, nil)
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate deleting a task that does not exist.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the new task %s.", testID, id)
+		{
+			if w.Code != http.StatusNotFound {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 404 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 204 for the response.", dbtest.Success, testID)
+		}
+	}
+}
+
+// putTask404 validates updating a task that does not exist.
+func (pt *TaskTests) putTask404(t *testing.T) {
+	id := "9b468f90-1cf1-4377-b3fa-68b450d632a0"
+
+	up := task.UpdateTask{
+		Name: dbtest.StringPointer("Nonexistent"),
+	}
+	body, err := json.Marshal(&up)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r := httptest.NewRequest(http.MethodPut, "/v1/task/"+id, bytes.NewBuffer(body))
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate updating a task that does not exist.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the new task %s.", testID, id)
+		{
+			if w.Code != http.StatusNotFound {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 404 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 404 for the response.", dbtest.Success, testID)
+
+			got := w.Body.String()
+			exp := "not found"
+			if !strings.Contains(got, exp) {
+				t.Logf("\t\tTest %d:\tGot : %v", testID, got)
+				t.Logf("\t\tTest %d:\tExp: %v", testID, exp)
+				t.Fatalf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Failed, testID)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Success, testID)
+		}
+	}
+}
+
+// crudTask performs a complete test of CRUD against the api.
+func (pt *TaskTests) crudTask(t *testing.T) {
+	p := pt.postTask201(t)
+	defer pt.deleteTask204(t, p.ID)
+
+	pt.getTask200(t, p.ID)
+	pt.putTask204(t, p.ID)
+}
+
+// postTask201 validates a task can be created with the endpoint.
+func (pt *TaskTests) postTask201(t *testing.T) task.Task {
+	np := task.NewTask{
+		Name: "Comic Books",
+		Pid:  "45cf87a3-5915-4079-a9af-6c559239ddbf",
+		Wid:  "7da3ca14-6366-47cf-b953-f706226567d8",
+	}
+
+	body, err := json.Marshal(&np)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r := httptest.NewRequest(http.MethodPost, "/v1/task", bytes.NewBuffer(body))
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	// This needs to be returned for other dbtest.
+	var got task.Task
+
+	t.Log("Given the need to create a new task with the tasks endpoint.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the declared task value.", testID)
+		{
+			if w.Code != http.StatusCreated {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 201 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 201 for the response.", dbtest.Success, testID)
+
+			if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to unmarshal the response : %v", dbtest.Failed, testID, err)
+			}
+
+			// Define what we wanted to receive. We will just trust the generated
+			// fields like ID and Dates so we copy p.
+			exp := got
+			exp.Name = "Comic Books"
+			exp.Wid = "7da3ca14-6366-47cf-b953-f706226567d8"
+			exp.Pid = "45cf87a3-5915-4079-a9af-6c559239ddbf"
+
+			if diff := cmp.Diff(got, exp); diff != "" {
+				t.Fatalf("\t%s\tTest %d:\tShould get the expected result. Diff:\n%s", dbtest.Failed, testID, diff)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get the expected result.", dbtest.Success, testID)
+		}
+	}
+
+	return got
+}
+
+// deleteTask200 validates deleting a task that does exist.
+func (pt *TaskTests) deleteTask204(t *testing.T, id string) {
+	r := httptest.NewRequest(http.MethodDelete, "/v1/task/"+id, nil)
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate deleting a task that does exist.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the new task %s.", testID, id)
+		{
+			if w.Code != http.StatusNoContent {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 204 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 204 for the response.", dbtest.Success, testID)
+		}
+	}
+}
+
+// getTask200 validates a task request for an existing id.
+func (pt *TaskTests) getTask200(t *testing.T, id string) {
+	r := httptest.NewRequest(http.MethodGet, "/v1/task/"+id, nil)
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to validate getting a task that exists.")
+	{
+		testID := 0
+		t.Logf("\tTest : %d\tWhen using the new task %s.", testID, id)
+		{
+			if w.Code != http.StatusOK {
+				t.Fatalf("\t%s\tTest : %d\tShould receive a status code of 200 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest : %d\tShould receive a status code of 200 for the response.", dbtest.Success, testID)
+
+			var got task.Task
+			if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+				t.Fatalf("\t%s\tTest : %d\tShould be able to unmarshal the response : %v", dbtest.Failed, testID, err)
+			}
+
+			// Define what we wanted to receive. We will just trust the generated
+			// fields like Dates so we copy p.
+			exp := got
+			exp.ID = id
+			exp.Name = "Comic Books"
+
+			if diff := cmp.Diff(got, exp); diff != "" {
+				t.Fatalf("\t%s\tTest : %d\tShould get the expected result. Diff:\n%s", dbtest.Failed, testID, diff)
+			}
+			t.Logf("\t%s\tTest : %d\tShould get the expected result.", dbtest.Success, testID)
+		}
+	}
+}
+
+// putTask204 validates updating a task that does exist.
+func (pt *TaskTests) putTask204(t *testing.T, id string) {
+	body := `{"name": "Graphic Novels"}`
+	r := httptest.NewRequest(http.MethodPut, "/v1/task/"+id, strings.NewReader(body))
+	w := httptest.NewRecorder()
+
+	r.Header.Set("Authorization", "Bearer "+pt.userToken)
+	pt.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to update a task with the tasks endpoint.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen using the modified task value.", testID)
+		{
+			if w.Code != http.StatusNoContent {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 204 for the response : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 204 for the response.", dbtest.Success, testID)
+
+			r = httptest.NewRequest(http.MethodGet, "/v1/task/"+id, nil)
+			w = httptest.NewRecorder()
+
+			r.Header.Set("Authorization", "Bearer "+pt.userToken)
+			pt.app.ServeHTTP(w, r)
+
+			if w.Code != http.StatusOK {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 200 for the retrieve : %v", dbtest.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 200 for the retrieve.", dbtest.Success, testID)
+
+			var ru task.Task
+			if err := json.NewDecoder(w.Body).Decode(&ru); err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to unmarshal the response : %v", dbtest.Failed, testID, err)
+			}
+
+			if ru.Name != "Graphic Novels" {
+				t.Fatalf("\t%s\tTest %d:\tShould see an updated Name : got %q want %q", dbtest.Failed, testID, ru.Name, "Graphic Novels")
+			}
+			t.Logf("\t%s\tTest %d:\tShould see an updated Name.", dbtest.Success, testID)
+		}
+	}
+}
