59056595f1f02a80cc0b3eb98fe9d3b53ab600bd
AhmedShaef <shaef@outlook.com>
2022-07-06 20:03:58 +0000

    add: a tag package comment

--- business/core/tag/tags.go
+++ business/core/tag/tags.go
@@ -1,3 +1,6 @@
+// Package tag provides an example of a core business API. Right now these
+// calls are just wrapping the data/data layer. But at some point you will
+// want auditing or something that isn't specific to the data/store layer.
 package tag
 
 import (
