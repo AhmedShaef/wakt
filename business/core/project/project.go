19b093b06808dffc27be8125a1bdce7f35337f8a
AhmedShaef <shaef@outlook.com>
2022-07-10 13:42:45 +0000

    fix: add project package comment

--- business/core/project/project.go
+++ business/core/project/project.go
@@ -1,4 +1,4 @@
-// Package projects provides an example of a core business API. Right now these
+// Package project provides an example of a core business API. Right now these
 // calls are just wrapping the data/data layer. But at some point you will
 // want auditing or something that isn't specific to the data/store layer.
 package project
